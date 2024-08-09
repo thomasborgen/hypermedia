1b88153ed5cc096a89160524ec04c8bf64b62943
Thomas Borgen <thomasborgen91@gmail.com>
2024-08-10 00:47:51 +0300

    All attributes and elements are updated

--- hypermedia/tables.py
+++ hypermedia/tables.py
@@ -1,79 +1,123 @@
-from hypermedia.models import BaseElement, VoidElement
+from typing import Unpack
+
+from hypermedia.models import Element, VoidElement
+from hypermedia.models.types import AnyChildren, ComplexChildren
+from hypermedia.types.attributes import ColAttrs, GlobalAttrs, TdAttrs, ThAttrs
 
 
-class Table(BaseElement):
+class Table(Element[ComplexChildren, GlobalAttrs]):
     """Defines a table."""
 
     tag: str = "table"
 
+    def __init__(
+        self, *children: ComplexChildren, **attributes: Unpack[GlobalAttrs]
+    ) -> None:
+        super().__init__(*children, **attributes)
 
-class Caption(BaseElement):
+
+class Caption(Element[AnyChildren, GlobalAttrs]):
     """Defines a table caption."""
 
     tag: str = "caption"
 
+    def __init__(
+        self, *children: AnyChildren, **attributes: Unpack[GlobalAttrs]
+    ) -> None:
+        super().__init__(*children, **attributes)
 
-class Th(BaseElement):
+
+class Th(Element[AnyChildren, ThAttrs]):
     """Defines a header cell in a table."""
 
     tag: str = "th"
+
+    def __init__(
+        self, *children: AnyChildren, **attributes: Unpack[ThAttrs]
+    ) -> None:
+        super().__init__(*children, **attributes)
 
 
 class TableHeader(Th):
     """Alias for `Th`."""
 
 
-class Tr(BaseElement):
+class Tr(Element[ComplexChildren, GlobalAttrs]):
     """Defines a row in a table."""
 
     tag: str = "tr"
+
+    def __init__(
+        self, *children: ComplexChildren, **attributes: Unpack[GlobalAttrs]
+    ) -> None:
+        super().__init__(*children, **attributes)
 
 
 class TableRow(Tr):
     """Alias for `Tr`."""
 
 
-class Td(BaseElement):
+class Td(Element[AnyChildren, TdAttrs]):
     """Defines a cell in a table."""
 
     tag: str = "td"
+
+    def __init__(
+        self, *children: AnyChildren, **attributes: Unpack[TdAttrs]
+    ) -> None:
+        super().__init__(*children, **attributes)
 
 
 class TableData(Td):
     """Alias for `Td`."""
 
 
-class THead(BaseElement):
+class THead(Element[ComplexChildren, GlobalAttrs]):
     """Groups the header content in a table."""
 
     tag: str = "thead"
+
+    def __init__(
+        self, *children: ComplexChildren, **attributes: Unpack[GlobalAttrs]
+    ) -> None:
+        super().__init__(*children, **attributes)
 
 
 class TableHead(THead):
     """Alias for `THead`."""
 
 
-class TBody(BaseElement):
+class TBody(Element[ComplexChildren, GlobalAttrs]):
     """Groups the body content in a table."""
 
     tag: str = "tbody"
+
+    def __init__(
+        self, *children: ComplexChildren, **attributes: Unpack[GlobalAttrs]
+    ) -> None:
+        super().__init__(*children, **attributes)
 
 
 class TableBody(TBody):
     """Alias for `TBody`."""
 
 
-class TFoot(BaseElement):
+class TFoot(Element[ComplexChildren, GlobalAttrs]):
     """Groups the footer content in a table."""
 
     tag: str = "tfoot"
+
+    def __init__(
+        self, *children: ComplexChildren, **attributes: Unpack[GlobalAttrs]
+    ) -> None:
+        super().__init__(*children, **attributes)
 
 
 class TableFoot(TFoot):
     """Alias for `TFoot`."""
 
 
-class Col(VoidElement):
+class Col(VoidElement[ColAttrs]):
     """
     Specifies column properties.
 
@@ -82,15 +126,23 @@ class Col(VoidElement):
 
     tag: str = "col"
 
+    def __init__(self, **attributes: Unpack[ColAttrs]) -> None:
+        super().__init__(**attributes)
+
 
 class Column(Col):
     """Alias for `Col`."""
 
 
-class ColGroup(BaseElement):
+class ColGroup(Element[AnyChildren, GlobalAttrs]):
     """Specifies a group of one or more columns in a table for formatting."""
 
     tag: str = "colgroup"
+
+    def __init__(
+        self, *children: AnyChildren, **attributes: Unpack[GlobalAttrs]
+    ) -> None:
+        super().__init__(*children, **attributes)
 
 
 class ColumnGroup(ColGroup):
